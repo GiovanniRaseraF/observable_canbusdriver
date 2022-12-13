2737a66b0bf649f1b33d47bec09dfc8dea5d628e
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2022-12-13 11:20:16 +0100

    testing: added looping file feature example

--- src/testing/replay.py
+++ src/testing/replay.py
@@ -3,13 +3,17 @@ sys.path.append('..')
 from canfilereplay import *
 
 if __name__ == "__main__":
+    log.basicConfig(level=log.INFO)
+    log.info("canreplay with 0.01s wait for data and file looping enabled")
 
     canreplay = canlistenfromfile(
         pathtofile="./toreplay.csv",
-        replayfrequency = 0.1
+        replayfrequency = 0.01,
+        loopfile = True
     )
 
     canreplay.add_listener(motor(desc="motor", on = [0x500, 0x460]))
+    canreplay.add_listener(motor(desc="battery", on = [0x500]))
 
     print(canreplay)
 
